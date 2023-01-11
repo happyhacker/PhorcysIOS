5d2d9c6a9c3eb02c1c3bd52757e1d63a05b61dcf
Larry Hack <Larry@Hacksoft.net>
2023-01-11 14:23:43 -0600

    forgot to commit for ages

---
+++ Phorcys/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Phorcys
+//
+//  Created by LARRY HACK on 7/1/14.
+//  Copyright (c) 2014 Hacksoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
